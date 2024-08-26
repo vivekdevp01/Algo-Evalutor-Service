8370e038b4dc325dead4acc14594ece2a715523d
Vivek Singh Pundir <132738914+vivekdevp01@users.noreply.github.com>
2024-08-26 23:07:09 +0530

    basic ioredis setup with bullmq

--- src/config/serverConfig.ts
+++ src/config/serverConfig.ts
@@ -3,5 +3,7 @@ import dotenv from  'dotenv';
 dotenv.config();
 
 export default{
-    PORT:process.env.PORT||3000
+    PORT:process.env.PORT||3000,
+    REDIS_PORT:process.env.REDIS_PORT,
+    REDIS_HOST:process.env.REDIS_HOST
 }
